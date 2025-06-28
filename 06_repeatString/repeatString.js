cfdd5e6b26579acfd2ef8e169817c9c5d962790b
Duckycolors <codeandwork23@protonmail.com>
2025-06-28 15:48:30 +1000

    Javascript exercise 6 complete

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
-};
+const repeatString = function(string, num) {
+    if (num < 0) return "ERROR";
+    let wordString = "";
+    for(let i = 0; i < num; i++) {
+        wordString += string;
+    }
+    return wordString;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
