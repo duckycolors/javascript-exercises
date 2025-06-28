b669c32bd61aa379369b540c2a0dc06b534c7bf3
Duckycolors <codeandwork23@protonmail.com>
2025-06-28 15:59:36 +1000

    Javascript exercise 7 complete

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    stringArray = string.split("");
+    reverseArray = stringArray.reverse();
+    joinArray = reverseArray.join("");
+    return joinArray;
 };
 
 // Do not edit below this line
