547e562b625cd1b44e4ab44f3d8826c6b4bd7703
Duckycolors <codeandwork23@protonmail.com>
2025-06-28 17:24:17 +1000

    Javascript exercise 9 complete with unknown error

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let smallValue = Math.min(num1, num2);
+    let bigValue = Math.max(num1, num2);
+    let sum = 0;
+    if (num1 <= 0) return "ERROR";
+    if (num2 <= 0) return "ERROR";
+    if ((typeof num1 !== typeof(1)) || (typeof num2 !== typeof(1))) {
+        return "ERROR";
+    } else
+    for(let i = smallValue; i <= bigValue; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
