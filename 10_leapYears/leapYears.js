69c2c27e006502b5cf1470101c70d6895960358b
Duckycolors <codeandwork23@protonmail.com>
2025-06-29 21:00:46 +1000

    Javascript exercise 10 complete

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,12 +1,11 @@
 const leapYears = function(year) {
-    if(year % 4 == 0) {
-        return true;
-    } else if (year % 400 && !year % 100){
+    if(year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)) {
         return true;
     } else {
         return false;
     }
-};
+    
+}
 
 // Do not edit below this line
 module.exports = leapYears;
