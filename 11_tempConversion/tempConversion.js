80380d0ea73cc83915000ac0ef0f255ad64964e7
Duckycolors <codeandwork23@protonmail.com>
2025-06-29 21:13:33 +1000

    Javascript exercise 11 complete

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,8 +1,14 @@
-const convertToCelsius = function() {
-};
+const convertToCelsius = function(num) {
+  let number = (num - 32) * 5/9;
+  let rounded = Math.round(number * 10) / 10;
+  return rounded;
+}
 
-const convertToFahrenheit = function() {
-};
+const convertToFahrenheit = function(num) {
+  let number = (num * 9/5) + 32;
+  let rounded = Math.round(number * 10) / 10;
+  return rounded;
+}
 
 // Do not edit below this line
 module.exports = {
